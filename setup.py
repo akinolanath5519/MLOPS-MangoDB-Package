a8cfd2bcc7fdf57218d63d325589a10a90e324d7
akinolanath <akinolanathaniel3026>
2024-06-26 22:49:09 -0500

    new folder

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup, find_packages
-from typing import List
 
 with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()     
-   
+    long_description = f.read()
 
 __version__ = "0.0.4"
 REPO_NAME = "MlopsMangoDBpackage"
-PKG_NAME= "mangoDBdatabaseautomation"
+PKG_NAME = "mangoDBdatabaseautomation"
 AUTHOR_USER_NAME = "akinolanath5519"
 AUTHOR_EMAIL = "akinolanathaniel3026@gmail.com"
+
+def parse_requirements(filename):
+    with open(filename, 'r') as f:
+        return [line.strip() for line in f if line and not line.startswith('#')]
 
 setup(
     name=PKG_NAME,
@@ -18,12 +20,12 @@ setup(
     author_email=AUTHOR_EMAIL,
     description="A python package for connecting with database.",
     long_description=long_description,
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires=get_requirement("./requirements_dev.txt"),
-    )
+    install_requires=parse_requirements("requirements_dev.txt"),
+)
--- src/mongodb_connect/mongo_crud.py
+++ src/mongodb_connect/mongo_crud.py
@@ -1,71 +1,62 @@
-from typing import Any
 import os
 import pandas as pd
-import pymongo
+from pymongo import MongoClient
 import json
 from ensure import ensure_annotations
+from typing import Any, Union
 
 
-from typing import Any
-import os
-import pandas as pd
-from pymongo.mongo_client import MongoClient
-import json
-from ensure import ensure_annotations
-
-
-class mongo_operation:
-    __collection=None # here i have created a private/protected variable
-    __database=None
+class MongoOperation:
+    __collection = None
+    __database = None
     
-    def __init__(self,client_url: str, database_name: str, collection_name: str=None):
-        self.client_url=client_url
-        self.database_name=database_name
-        self.collection_name=collection_name
-       
-    def create_mongo_client(self,collection=None):
-        client=MongoClient(self.client_url)
+    def __init__(self, client_url: str, database_name: str, collection_name: str = None):
+        self.client_url = client_url
+        self.database_name = database_name
+        self.collection_name = collection_name
+
+    def create_mongo_client(self) -> MongoClient:
+        client = MongoClient(self.client_url)
         return client
     
-    def create_database(self,collection=None):
-        if mongo_operation.__database==None:
-            client=self.create_mongo_client(collection)
-            self.database=client[self.database_name]
-        return self.database 
+    def create_database(self) -> Any:
+        if MongoOperation.__database is None:
+            client = self.create_mongo_client()
+            self.database = client[self.database_name]
+            MongoOperation.__database = self.database
+        return self.database
     
-    def create_collection(self,collection=None):
-        if mongo_operation.__collection==None:
-            database=self.create_database(collection)
-            self.collection=database[self.collection_name]
-            mongo_operation.__collection=collection
+    def create_collection(self, collection_name: str = None) -> Any:
+        if collection_name is None:
+            collection_name = self.collection_name
         
-        if mongo_operation.__collection!=collection:
-            database=self.create_database(collection)
-            self.collection=database[self.collection_name]
-            mongo_operation.__collection=collection
-            
+        if MongoOperation.__collection is None or MongoOperation.__collection != collection_name:
+            database = self.create_database()
+            self.collection = database[collection_name]
+            MongoOperation.__collection = self.collection
         return self.collection
     
-    def insert_record(self,record: dict, collection_name: str) -> Any:
-        if type(record) == list:
-            for data in record:
-                if type(data) != dict:
-                    raise TypeError("record must be in the dict")    
-            collection=self.create_collection(collection_name)
-            collection.insert_many(record)
-        elif type(record)==dict:
-            collection=self.create_collection(collection_name)
-            collection.insert_one(record)
-    
-    def bulk_insert(self,datafile,collection_name:str=None):
-        self.path=datafile
+    def insert_record(self, record: Union[dict, list], collection_name: str) -> None:
+        collection = self.create_collection(collection_name)
         
-        if self.path.endswith('.csv'):
-            pd.read.csv(self.path,encoding='utf-8')
-            
-        elif self.path.endswith(".xlsx"):
-            dataframe=pd.read_excel(self.path,encoding='utf-8')
-            
-        datajson=json.loads(dataframe.to_json(orient='record'))
-        collection=self.create_collection()
+        if isinstance(record, list):
+            for data in record:
+                if not isinstance(data, dict):
+                    raise TypeError("Each record must be a dict")
+            collection.insert_many(record)
+        elif isinstance(record, dict):
+            collection.insert_one(record)
+        else:
+            raise TypeError("Record must be a dict or a list of dicts")
+    
+    def bulk_insert(self, datafile: str, collection_name: str = None) -> None:
+        if datafile.endswith('.csv'):
+            dataframe = pd.read_csv(datafile, encoding='utf-8')
+        elif datafile.endswith(".xlsx"):
+            dataframe = pd.read_excel(datafile)
+        else:
+            raise ValueError("File must be a CSV or Excel file")
+        
+        datajson = json.loads(dataframe.to_json(orient='records'))
+        collection = self.create_collection(collection_name)
         collection.insert_many(datajson)
