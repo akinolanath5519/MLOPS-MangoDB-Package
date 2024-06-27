102682682bcd3b7390bb010203b9a3d78465ca55
akinolanath <akinolanathaniel3026>
2024-06-26 23:09:58 -0500

    new file

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+import os
 
 def get_requirements(file_name: str) -> list:
     with open(file_name, 'r') as f:
@@ -7,25 +8,22 @@ def get_requirements(file_name: str) -> list:
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
-__version__ = "0.0.4"
-REPO_NAME = "MlopsMangoDBpackage"
-PKG_NAME = "mangoDBdatabaseautomation"
-AUTHOR_USER_NAME = "akinolanath5519"
-AUTHOR_EMAIL = "akinolanathaniel3026@gmail.com"
-
 setup(
-    name=PKG_NAME,
-    version=__version__,
-    author=AUTHOR_USER_NAME,
-    author_email=AUTHOR_EMAIL,
-    description="A python package for connecting with database.",
+    name="mongodb_connect",
+    version="0.0.4",
+    author="akinolanath5519",
+    author_email="akinolanathaniel3026@gmail.com",
+    description="A python package for connecting with MongoDB database.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    url="https://github.com/akinolanath5519/MlopsMangoDBpackage",
     project_urls={
-        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+        "Bug Tracker": "https://github.com/akinolanath5519/MlopsMangoDBpackage/issues",
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=get_requirements("requirements.txt"),
+    extras_require={
+        'dev': get_requirements('requirements_dev.txt')
+    }
 )
